d6e2010b0e1d5f0693e87baf2a65f4aaed289ddb
kirtigarg11 <40167262+kirtigarg11@users.noreply.github.com>
2018-11-09 20:45:56 -0800

    Create quick.cpp

---
+++ quick.cpp
@@ -0,0 +1,70 @@
+
+
+#include<stdio.h> 
+  
+ 
+void swap(int* a, int* b) 
+{ 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+int partition (int arr[], int low, int high) 
+{ 
+    int pivot = arr[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j] <= pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+  
+
+void quickSort(int arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        int pi = partition(arr, low, high); 
+  
+    
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+     
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    int n;
+   scanf("%d",&n);
+
+    int arr[n];
+   for(int i=0;i<n;i++){
+scanf("%d",&arr[i]);
+   
+   
+} 
+    quickSort(arr, 0, n-1); 
+     
+    printArray(arr, n); 
+    return 0; 
+}
