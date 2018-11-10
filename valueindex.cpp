cc50b60b19984ac2debbdb60dcb23c3775657af7
kirtigarg11 <40167262+kirtigarg11@users.noreply.github.com>
2018-11-10 08:04:42 -0800

    Create valueindex.cpp

---
+++ valueindex.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+int main(){ 
+int n;
+cin>>n;
+int ar[n],i;
+for(i=0;i<n;i++){
+cin>>ar[i];
+cout<<ar[i]<<" "<<i<<endl;
+}
+}
