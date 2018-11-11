2eb03b88ecfce8a4dd03fe64ba8c60d098348e17
kirtigarg11 <40167262+kirtigarg11@users.noreply.github.com>
2018-11-10 22:26:47 -0800

    Create hr.cpp

---
+++ hr.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<sstream>
+#include<cmath>
+#include<string>
+#include<cstdlib>
+using namespace std;
+int main(){
+int h1,m1,h2,m2;
+cin>>h1>>m1;
+cin>>h2>>m2;
+int fh,fm;
+fh=abs(h2-h1);
+fm=abs(m2-m1);
+cout<<fh<<" "<<fm;
+
+
+
+
+}
